f853c0f23fd2f24eeb1f72fb6fd8533c96bb04ec
Diggo <rdiggosilva@gmail.com>
2024-01-23 16:14:12 -0300

    Sem sucesso

--- Appetizers/Resources/AppDelegate.swift
+++ Appetizers/Resources/AppDelegate.swift
@@ -11,7 +11,9 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        listaProdutos.value = []
+        print(listaProdutos.value = [])
+        listaProdutos.bind(observer: <#T##([Appetizer]?) -> ()#>)
+            
         return true
     }
 
