cb0ce9607ae2787631bdbe8b288889f81301b028
Diggo Silva <rdiggosilva@gmail.com>
2024-12-13 17:20:12 -0300

    Starting tests for AppetizerDetailViewModel

---
+++ AppetizersTests/AppetizerDetailViewModelTests.swift
@@ -0,0 +1,16 @@
+//
+//  AppetizerDetailViewModelTests.swift
+//  AppetizersTests
+//
+//  Created by Diggo Silva on 13/12/24.
+//
+
+import XCTest
+@testable import Appetizers
+
+final class AppetizerDetailViewModelTests: XCTestCase {
+    func testBlah() {
+        let valor = 1 == 1
+        XCTAssertTrue(valor)
+    }
+}
